a01c331c6f25ed55b83e106751502e8642e658b8
Sergei Antipov <greendayonfire@gmail.com>
2018-06-21 14:40:13 -0400

    Add kube-state-metrics deployment

--- resources/install.sh
+++ resources/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 password=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 | tr -d '\n ' | /opt/bin/base64)
 sed -i s/cGFzc3dvcmQtZ29lcy1oZXJlCg==/$password/g /var/lib/gravity/resources/grafana.yaml
-for filename in security smtp influxdb grafana heapster kapacitor telegraf alerts
+for filename in security smtp influxdb grafana heapster kapacitor telegraf alerts kube-state-metrics
 do
     /opt/bin/kubectl create -f /var/lib/gravity/resources/${filename}.yaml
 done
